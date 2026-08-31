d7f3fcfdca5c1d82f31478ebe438f343aa3bf819
mbasa batyi <mbasabatyi@icloud.com>
2026-08-31 17:58:21 +0200

    Add files via upload

---
+++ Lessons/lesson1.sql
@@ -0,0 +1,18 @@
+SET SERVEROUTPUT ON;
+
+/* 
+DECLARE                                                                         it is used to initialis, create variables and create constants
+    v_fname VARCHAR2(20);
+    
+BEGIN
+    SELECT first_name
+    INTO v_fname
+    FROM HR.employees
+    WHERE employee_id = 100;
+
+    DBMS_OUTPUT.PUT_LINE('The first name of the employee is ' || v_fname);
+END;
+*/
+
+
+
